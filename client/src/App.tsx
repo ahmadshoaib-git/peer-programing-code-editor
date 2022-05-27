cacffa17c67a21e8b8ec0339f7cb369709603db4
Ahmad Shoaib <ahmad.shoaib88@gmail.com>
2022-05-27 10:00:16 +0500

    chore: app.js and index.js cleaned, strict mode removed to stop rerendering.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,24 +4,15 @@ import Theme from "src/theme";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import GlobalStyle from "src/assets/styles/global.style";
-// import Editor from "./pages/Editor";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
 import Routes from "src/routes";
-// import Notify from "src/components/Notification";
 import "react-toastify/ReactToastify.min.css";
 
 function App() {
-  // React.useEffect(() => {
-  //   Notify("Yahooo!!!!", "info");
-  // }, []);
+  console.log("===> App");
   return (
     <Provider store={store}>
       <Theme>
         <GlobalStyle />
-        {/* <Editor /> */}
-        {/* <Login /> */}
-        {/* <Home /> */}
         <Routes />
         <ToastContainer position="bottom-right" newestOnTop />
       </Theme>
