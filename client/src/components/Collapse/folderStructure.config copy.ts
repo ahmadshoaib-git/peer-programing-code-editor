6b0df7aaa5dcad489d86bce2e11b10ed6bcd845b
Ahmad Shoaib <ahmad.shoaib88@gmail.com>
2022-05-30 11:44:51 +0500

    chore: minor changes added.

---
+++ client/src/components/Collapse/folderStructure.config copy.ts
@@ -0,0 +1,11 @@
+const structure = [
+  {
+    type: "folder",
+    name: "src",
+    files: [
+      { type: "file", name: "index.js" },
+      { type: "file", name: "app.js" },
+    ],
+  },
+];
+export { structure };
