32683a051f96b981695536b66c7a241c1dc090fb
Ahmad Shoaib <ahmad.shoaib88@gmail.com>
2022-06-13 19:27:52 +0500

    chore: minor modification for production.

--- src/index.ts
+++ src/index.ts
@@ -35,10 +35,12 @@ app.use("/api/project", ProjectRouter);
 
 export { mongoose };
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-app.get("/", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(process.env.NODE_ENV);
