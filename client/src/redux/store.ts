c81fc3464be41c04a9ba58469fc325f2026cd5ca
Ahmad Shoaib <ahmad.shoaib88@gmail.com>
2022-06-13 15:25:59 +0500

    chore: minor redux-logger modification for production.

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,13 +1,13 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 // import logger from "redux-logger";
-import { createLogger } from "redux-logger";
+// import { createLogger } from "redux-logger";
 import allReducers from "./slices";
 
 const rootReducer = combineReducers(allReducers);
-const logger = createLogger();
+// const logger = createLogger();
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== "production",
 });
 
